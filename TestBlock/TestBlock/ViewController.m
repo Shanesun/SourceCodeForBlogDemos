e2d9912761019a6e3af3a71e4cca803fbd47870a
zhiqiang.li <zhiqiang.li@tiejin.me>
2018-10-18 19:13:14 +0800

    mod TestBlock project

--- TestBlock/TestBlock/ViewController.m
+++ TestBlock/TestBlock/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSMutableArray *tmp;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
--- TestBlock/TestBlock/main.m
+++ TestBlock/TestBlock/main.m
@@ -6,14 +6,27 @@
 //  Copyright © 2018 Shane. All rights reserved.
 //
 
-#import <stdio.h>
+#import <Foundation/Foundation.h>
+
+static int outTmpVal = 30;
 
 int main(int argc, char * argv[]) {
     int tmpVal = 10;
+    static int localTmpVal = 20;
+    NSMutableArray *localMutArray = [NSMutableArray new];
+    
     void (^blk)(void) = ^{
-        printf("val = %d", tmpVal); // val = 10
+        printf("val = %d\n", tmpVal); // val = 10
+        printf("localTmpVal = %d\n", localTmpVal); // localTmpVal = 21
+        printf("outTmpVal = %d\n", outTmpVal); // outTmpVal = 31
+
+        [localMutArray addObject:@"newObj"];
+        printf("localMutArray.count = %d\n", (int)localMutArray.count); // localMutArray.count = 2
     };
     
     tmpVal = 2;
+    localTmpVal = 21;
+    outTmpVal = 31;
+    [localMutArray addObject:@"startObj"];
     blk();
 }
